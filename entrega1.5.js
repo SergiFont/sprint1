3bf6faab692c973526af608892fcec0c2a731f92
SergiFont <sfontsmx2010@gmail.com>
2023-02-08 23:52:05 +0100

    correciones y mejoras en los ejercicios, nivel 3 casi completado

--- entrega1.5.js
+++ entrega1.5.js
@@ -1,65 +1,65 @@
-// const fs = require('fs');
-// const { pipeline } = require('stream');
-// const zlib = require('zlib')
-// const { exec } = require('child_process');
-// const crypto = require('crypto')
 import fs from "fs"
-import {pipeline} from "stream"
+import { pipeline } from "stream"
 import zlib from "zlib"
-import {exec} from "child_process"
+import { exec } from "child_process"
 import crypto from "crypto"
 
 //Nivel 1, ejercicio 1
 /*Crea una funció que, en executar-la, escrigui una frase en un fitxer. */
 
-const writeWords = text => {
-        fs.writeFile('./texto-nivel1.txt', text, error => {
-            if(error) {
-                console.log(error)
-            }
-        })
+const writeWords = (text, fileName) => {
+    fs.writeFile(fileName, text, error => {
+        if (error) {
+            console.log(error)
+        }
+    })
 }
 
-writeWords('Hola! Felicidades por llegar hasta aquí!')
+writeWords('Hola! Felicidades por llegar hasta aquí!', './texto-nivel1.txt')
 
 //Nivel 1, ejercicio 2
 /*Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior. */
 
-const printFileContent = () => {
-    fs.readFile('./texto-nivel1.txt', 'utf8', (error, data) => {
-        if (error) throw error
-        console.log(data)
-    })
+const printFileContent = fileName => {
+    setTimeout(() => {
+        fs.readFile(fileName, 'utf8', (error, data) => {
+            if (error) throw error
+            console.log(data)
+        })
+    }, 500)
 }
-printFileContent()
+printFileContent('./texto-nivel1.txt')
+
+
 
 //Nivel 1, ejercicio 3
 /*Crea una funció que comprimeixi el fitxer del nivell 1. */
 
-const gzip = zlib.createGzip()
-const source = fs.createReadStream('texto-nivel1.txt')
-const destination = fs.createWriteStream('texto-nivel1.txt.gz')
+const compress = (gzip, source, destination) => {
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error(err)
+        }
+    })
+}
 
-pipeline(source, gzip, destination, (err) => {
-    if(err) {
-        console.error(err)
-    }
-})
+compress(zlib.createGzip(), fs.createReadStream('texto-nivel1.txt'), fs.createWriteStream('texto-nivel1.txt.gz'))
+
 
 //Nivel 2, ejercicio 1
 /*Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon. */
 
 const printCountdown = number => {
-    
-        console.log(number)
-    
-        const newNumber = number - 1
-        if (newNumber > 0) {
-            setTimeout(() => {
-                printCountdown(newNumber)
-            }, 1000)
-        }
+
+    console.log(number)
+
+    const newNumber = number - 1
+    if (newNumber > 0) {
+        setTimeout(() => {
+            printCountdown(newNumber)
+        }, 1000)
     }
+}
 
 printCountdown(10)
 // mirar setInterval
@@ -68,90 +68,99 @@ printCountdown(10)
 /*Crea una funció que llisti per la consola el contingut del directori d'usuari/ària de l'ordinador 
 utilizant Node Child Processes. */
 
-exec('dir', (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`)
-        return
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`)
-        return
-    }
-    console.log(`stdout: ${stdout}`)
-})
+const printDir = directory => {
+    exec(`cd ${directory} && dir`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`)
+            return
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`)
+            return
+        }
+        console.log(`stdout: ${stdout}`)
+    })
+}
+
+printDir('C:/Users')
 
 //Nivel 3, ejercicio 1
 /*Crea una funció que creï dos fitxers codificats en hexadecimal i en base64 respectivament, 
 a partir del fitxer del nivell 1.
-TODO Crea una funció que guardi els fitxers del punt anterior, ara encriptats amb l'algoritme aes-192-cbc, 
+Crea una funció que guardi els fitxers del punt anterior, ara encriptats amb l'algoritme aes-192-cbc, 
 i esborri els fitxers inicials. 
 TODO Crea una altra funció que desencripti i descodifiqui els fitxers de l'apartat anterior tornant a 
 generar una còpia de l'inicial.
 Inclou un README amb instruccions per a l'execució de cada part.*/
 
-// const data = fs.readFile('./texto-nivel1.txt', 'utf8', (error, data) => {
-//     if(error) return console.log(error)
-//     return data
-// })
+const data = fs.readFile('./texto-nivel1.txt', 'utf8', (error, data) => {
+    if (error) return console.log(error)
+    return data
+})
 
-// const encodeFile = encoding => {
-//     const buffer = Buffer.from(String(data)).toString(encoding)
-//     fs.writeFile(`./texto-nivel1-${encoding}.txt`, buffer, error => {
-//         if(error) {
-//             console.log(error)
-//         }
-//     })
-//     console.log(data)// TODO EL PROBLEMA ESTA AQUI!!!!!!! comparar con test
-// }
-// const hex = encodeFile('hex')
-// const base64 = encodeFile('base64')
+const encodeFile = encoding => {
+    const buffer = Buffer.from(String(data)).toString(encoding)
+    fs.writeFile(`./texto-nivel1-${encoding}.txt`, buffer, error => {
+        if (error) {
+            console.log(error)
+        }
+    })
+}
 
-// // const KEY = Buffer.from(crypto.randomBytes(24), "hex")
-// // const IV = Buffer.from(crypto.randomBytes(12), "hex")
-// const KEY = Buffer.from("6a97a88e993b645c315a52d810617641d311dbde52cda19f", "hex")
-// const IV =  Buffer.from("abe5d902bb2d112f8925a93f", "hex")
+encodeFile('hex')
+encodeFile('base64')
 
-// const cipherFiles = file => {
-//     const cipher = crypto.createCipheriv('aes-192-ccm', KEY, IV, {authTagLength: 12})
-//     let encrypted = cipher.update(file, 'utf8', 'base64')
-//     encrypted += cipher.final('base64')
-//     return (encrypted)
-// }
-    
+const KEY = Buffer.from("6a97a88e993b645c315a52d810617641d311dbde52cda19f", "hex")
+const IV = Buffer.from("abe5d902bb2d112f8925a93f", "hex")
 
-// const decipherFiles = file => {
-//     const descipher = crypto.createDecipheriv('aes-192-ccm', KEY, IV, {authTagLength: 12})
-//     let result = descipher.update(file, 'base64', 'utf8')
-//     result += descipher.final('utf8')
-//     return result
-// }
+const cipherFiles = (file, encoding) => {
+    const cipher = crypto.createCipheriv('aes-192-ccm', KEY, IV, { authTagLength: 12 })
+    let encrypted = cipher.update(file, 'utf8', encoding)
+    encrypted += cipher.final(encoding)
+    return encrypted
+}
 
-// const createAndDelete = file => {
-//     fs.writeFile(`./archivo-nivel3-aes-192-cipher.txt`, cipherFiles(String(file)), error => {
-//         if(error) {
-//             console.log(error)
-//         }
-//     })
+const encryptDocument = encoding => {
+    setTimeout(() => {
+        fs.readFile(`./texto-nivel1-${encoding}.txt`, 'utf8', (error, data) => {
+            if (error) return console.log(error)
 
-//     exec('del "texto-nivel1-base64.txt"', (error, stdout, stderr) => { // TODO cambiar a comando para borrar archivo
-//         if (error) {
-//             console.log(`error: ${error.message}`)
-//             return
-//         }
-//         if (stderr) {
-//             console.log(`stderr: ${stderr}`)
-//             return
-//         }
-//         console.log(`File deleted succesfully`)
-//     })
-// }
+            const buffer = Buffer.from(String(cipherFiles(data, encoding))).toString(encoding)
 
-// // const firstFile = fs.readFile('./archivo-nivel3-aes-192-cipher.txt', 'utf8', (error, data) => {
-// //     if (error) throw error
-// //     return (data)
-// // })
+            fs.writeFile(`./texto-nivel1-${encoding}-encrypted.txt`, buffer, error => {
+                if (error) console.log(error)
 
-// // console.log(firstFile)
+                fs.unlink(`./texto-nivel1-${encoding}.txt`, error => {
+                    if (error) console.log(error)
+                })
+            })
+        })
+    }, 500)
+}
 
-// createAndDelete(hex)
-// createAndDelete(base64)
+encryptDocument('base64')
+encryptDocument('hex')
+
+const decipherFiles = (file, encoding) => {
+    const descipher = crypto.createDecipheriv('aes-192-ccm', KEY, IV, {authTagLength: 12})
+    let decrypted = descipher.update(file, encoding, 'utf8')
+    decrypted += descipher.final('utf8')
+    return decrypted
+}
+
+const uncryptDocument = encoding => {
+    setTimeout(() => {
+        fs.readFile(`./texto-nivel1-${encoding}-encrypted.txt`, 'utf8', (error, data) => {
+            if (error) return console.log(error)
+
+            const buffer = Buffer.from(String(decipherFiles(data, encoding))).toString(encoding)
+
+            fs.writeFile(`./texto-nivel1-${encoding}-copy.txt`, buffer, error => {
+                if (error) console.log(error)
+            })
+        })
+    }, 700)
+}
+
+uncryptDocument('base64')
+uncryptDocument('hex')
